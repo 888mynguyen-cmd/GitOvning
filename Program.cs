7f62034f0bd390cb64001b2901ab8c901e3ac61c
My Nguyen <888mynguyen@gmail.com>
2026-09-25 13:06:35 +0200

    Ändring från originalprojektet

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Välkommen till Git-övningen!");
 Console.WriteLine($"Idag är det {DateTime.Now:yyyy-MM-dd}");
+Console.WriteLine("Ändring från originalprojektet.");
