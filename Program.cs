e7403705b9512a667127933e29ebcb14085d03fa
My Nguyen <888mynguyen@gmail.com>
2026-09-25 12:28:36 +0200

    Revert "Lägg till felaktig ändring"
    This reverts commit d9d720c65fe81688aa2eaa61287064b9aa6fdbef.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Välkommen till Git-övningen!");
 Console.WriteLine($"Idag är det {DateTime.Now:yyyy-MM-dd}");
-Console.WriteLine("Detta är en felaktig ändring.");
