d9d720c65fe81688aa2eaa61287064b9aa6fdbef
My Nguyen <888mynguyen@gmail.com>
2026-09-25 12:27:58 +0200

    Lägg till felaktig ändring

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Välkommen till Git-övningen!");
 Console.WriteLine($"Idag är det {DateTime.Now:yyyy-MM-dd}");
+Console.WriteLine("Detta är en felaktig ändring.");
